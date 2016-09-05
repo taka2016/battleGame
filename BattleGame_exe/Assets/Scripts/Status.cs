2fede936cf47a9aeee0ab549264fcb8c119a46b5
oka_takashi\takashi <ialwaypositive@gmail.com>
2016-09-06 01:53:18 +0900

    BattleGame_exe

---
+++ BattleGame_exe/Assets/Scripts/Status.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Status : MonoBehaviour {
+
+    public int hp;
+    public int str;
+    public int def;
+    public int chance_per;
+    public int atk_per;
+    public int def_per;
+    public int magic_per;
+
+}
